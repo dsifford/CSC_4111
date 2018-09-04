aebfc83d9cb23f3bee6803d90566ebea5f5e671b
Derek P Sifford <dereksifford@gmail.com>
2018-09-04 18:30:37 -0400

    lab 1 progress

--- lab_01/task_02/defense.h
+++ lab_01/task_02/defense.h
@@ -1,10 +1,18 @@
 #ifndef DEFENSE_H
 #define DEFENSE_H
 
+#include <string>
 #include "player.h"
 
 class Defense: public Player {
- // TODO: ...
-}
+	public:
+		int tackles;
+		Defense(string name);
+		void setMinutes(int minutes);
+		void setTackles(int tackles);
+		void printStats() const;
+	private:
+		string name;
+};
 
 #endif
--- lab_01/task_02/offense.h
+++ lab_01/task_02/offense.h
@@ -1,10 +1,19 @@
 #ifndef OFFENSE_H
 #define OFFENSE_H
 
+#include <string>
+
 #include "player.h"
 
 class Offense: public Player {
-	// TODO...
-}
+	public:
+		int yards;
+		Offense(string name);
+		void setMinutesPlayed(int minutes);
+		void setYards(int yards);
+		void printStats() const;
+	private:
+		string name;
+};
 
 #endif
